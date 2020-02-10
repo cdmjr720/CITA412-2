70ffad5b793e8db5a1fbbb5aacde32a2ae95ffbd
cdmjr720 <charlesdmaitland@gmail.com>
2020-02-10 16:46:37 -0500

    Fixed Game - "New" initial commit

---
+++ Assets/Scripts/ActionButtons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionButtons : MonoBehaviour
+{
+    int start = 1;
+
+    public void OnPressPest()
+    {
+        Press();
+    }
+
+    public void OnPressSci()
+    {
+        Press();
+    }
+
+    void Press()
+    {
+        int press = Random.Range(-500, 500);
+        if (press >= -500)
+        {
+            Debug.Log(press);
+        }
+    }
+}
