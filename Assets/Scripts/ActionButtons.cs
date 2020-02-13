864c30cdaf0d3964532e96e52611ed7325f7e15d
cdmjr720 <charlesdmaitland@gmail.com>
2020-02-12 19:31:37 -0500

    Buttons disabling appropriately

--- Assets/Scripts/ActionButtons.cs
+++ Assets/Scripts/ActionButtons.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ActionButtons : MonoBehaviour
 { 
     private float timeSinceClicked = Mathf.Infinity;
 
+    [SerializeField] GameObject button;
+
+    //[SerializeField] Button sciButton;
+    //GameObject button;
+    
     public void OnPressPest()
     {
         Press();
         timeSinceClicked = 0f;
+        button.SetActive(false);
 
     }
 
@@ -42,6 +49,10 @@ public class ActionButtons : MonoBehaviour
     public void Update()
     {
         timeSinceClicked += Time.deltaTime;
+        if (timeSinceClicked >4f && !button.active)
+        {
+            button.SetActive(true);
+        }
     }
 
 
